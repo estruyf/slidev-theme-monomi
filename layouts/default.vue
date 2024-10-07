d86ce2a22b4359c9dfe6dbe362ee21a269b4a41b
Elio Struyf <elio@struyfconsulting.be>
2024-10-07 10:49:01 +0200

    Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  const props = defineProps<{
+    invert?: boolean
+  }>()
+
+  const invertClass = computed(() => props.invert ? "inverted" : "")
+</script>
+
+<template>
+  <div class="slidev-layout default" :class="invertClass">
+    <slot />
+  </div>
+</template>
